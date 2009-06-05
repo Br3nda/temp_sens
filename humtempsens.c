64d8e7e520f76c1ef9714d520732015ecddc17b2
Timothy Goddard <tim@goddard.net.nz>
2009-06-06 02:41:22 +1200

    Add CRC check to temperature

--- humtempsens.c
+++ humtempsens.c
@@ -35,78 +35,89 @@ static inline void printBin (uint16_t input, uint8_t bits) {
 }
 
 static inline int16_t humtempsens_measure(uint8_t command) {
-  humtempsens_command_init();
   
-//  uint8_t crc = 0;
-  uint8_t bit;
-  for (bit = 0; bit < 8; bit++) {
-    if (command >> 7) {
-      DATA_UP();
-    } else {
-      DATA_DOWN();
-    }
-    SCK_UP();
-//    uint8_t txb = (command ^ crc) >> 7;
-//     crc <<= 1;
-//     crc |= txb;
-//     crc ^= ((txb << 4) | (txb << 5));
-    SCK_DOWN();
-    command <<= 1;
-  }
   
-  // Check acknowledgement
-  DATA_UP();
-  if (PIND & (1 << PD7)) {
-    rprintf("Failed to receive acknowledgement.\n");
-    return -1;
-  }
+  uint8_t crc, checksum;
+  uint16_t val;
+  do {
   
-  SCK_UP();
-  SCK_DOWN();
-  
-  // Wait for data ready
-  while (PIND & (1 << PD7)) {}
-  
-  // Retrieve data
-  uint8_t byte;
-  uint16_t val = 0;
-  for (byte = 0; byte < 2; byte++) {
+    crc = 0;
+    
+    // Send command
+    humtempsens_command_init();
+    uint8_t bit;
     for (bit = 0; bit < 8; bit++) {
+      if (command >> 7) {
+        DATA_UP();
+      } else {
+        DATA_DOWN();
+      }
       SCK_UP();
-      val <<= 1;
-      uint8_t in = (PIND >> PD7) & 1;
+      uint8_t txb = (command ^ crc) >> 7;
+      crc <<= 1;
+      crc |= txb;
+      crc ^= ((txb << 4) | (txb << 5));
+      SCK_DOWN();
+      command <<= 1;
+    }
+    
+    // Check acknowledgement
+    DATA_UP();
+    if (PIND & (1 << PD7)) {
+      rprintf("Failed to receive acknowledgement.\n");
+      return -1;
+    }
+    
+    SCK_UP();
+    SCK_DOWN();
+    
+    // Wait for data ready
+    while (PIND & (1 << PD7)) {}
+    
+    // Retrieve data
+    uint8_t byte;
+    for (byte = 0; byte < 2; byte++) {
+      for (bit = 0; bit < 8; bit++) {
+        val <<= 1;
+        uint8_t in = (PIND >> PD7) & 1;
+        
+        val |= in;
+        uint8_t txb = in ^ (crc >> 7);
+        crc <<= 1;
+        crc |= txb;
+        crc ^= ((txb << 4) | (txb << 5));
+        
+        SCK_UP();
+        SCK_DOWN();
+      }
       
-      val |= in;
-//      uint8_t txb = in ^ (crc >> 7);
-//       crc <<= 1;
-//       crc |= txb;
-//       crc ^= ((txb << 4) | (txb << 5));
+      // Acknowledge
+      DATA_DOWN();
+      SCK_UP();
+      SCK_DOWN();
+      DATA_UP();
+    }
+
+    // Retrieve checksum
+    checksum = 0;
+    for (bit = 0; bit < 8; bit++) {
+      checksum >>= 1;
+      if (PIND & (1 << PD7)) {
+        checksum |= (1 << 7);
+      } else {
+      }
+      SCK_UP();
       SCK_DOWN();
     }
     
-    // Acknowledge
-    DATA_DOWN();
-    SCK_UP();
-    SCK_DOWN();
-    DATA_UP();
-  }
-
-// Checksum calculation is currently not working
-  // Retrieve checksum
-  uint8_t checksum = 0;
-//   rprintf("Checksum: ");
-  for (bit = 0; bit < 8; bit++) {
-    SCK_UP();
-    SCK_DOWN();
-    checksum <<= 1;
-    if (PIND & (1 << PD7)) {
-      checksum |= 1;
-//       rprintfChar('1');
-    } else {
-//       rprintfChar('0');
-    }
-  }
-//   rprintfChar('\n');
+    if (checksum != crc) humtempsens_init();
+  } while (checksum != crc);
+  
+  // Acknowledge
+  DATA_DOWN();
+  SCK_UP();
+  SCK_DOWN();
+  DATA_UP();
   
   return val;
 }
@@ -114,6 +125,12 @@ static inline int16_t humtempsens_measure(uint8_t command) {
 void humtempsens_init( void ) {
   PORTD &= ~(1 << PD7);
   DDRD |= (1 << PD6);
+  
+  uint8_t pulses;
+  for (pulses = 0; pulses < 10; pulses++) {
+    SCK_UP();
+    SCK_DOWN();
+  }
   
   humtempsens_command_init();
   
